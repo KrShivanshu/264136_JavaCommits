637520a277867d4f69473ae02d326d6ed21bfef3
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-07-27 11:34:40 +0530

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -1,35 +1,36 @@
-import java.util.*; 
+import java.util.*;
 
 class Bowling {
     private String s;
     private int max = 0;
     HashMap<String, Integer> players;
+
     Bowling() {
         players = new HashMap<String, Integer>();
     }
+
     public void addPlayer(String name, int p) {
         players.put(name, p);
     }
 
-    public void getWinner(){
-    for (Map.Entry<String, Integer> set :
-             players.entrySet()){
-                if(set.getValue()>max){
-                    max= set.getValue();
-                    s = set.getKey();
-                }
+    public void getWinner() {
+        for (Map.Entry<String, Integer> set : players.entrySet()) {
+            if (set.getValue() > max) {
+                max = set.getValue();
+                s = set.getKey();
             }
-            System.out.println(s);
         }
-            
+        System.out.println(s);
+    }
+
 }
 
 public class Program {
-    public static void main(String[ ] args) {
+    public static void main(String[] args) {
         Bowling game = new Bowling();
         Scanner sc = new Scanner(System.in);
 
-        for(int i=0;i<3;i++) {
+        for (int i = 0; i < 3; i++) {
             String input = sc.nextLine();
             String[] values = input.split(" ");
             String name = values[0];
@@ -37,5 +38,6 @@ public class Program {
             game.addPlayer(name, points);
         }
         game.getWinner();
+        sc.close();
     }
 }
